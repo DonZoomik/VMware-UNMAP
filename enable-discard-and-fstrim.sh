767dc48b12a7d272a4e13c40c6c9e5a2aeef7eaa
Mihkel Soomere <mihkel@soomere.ee>
2026-04-02 15:06:21 +0300

    randomness in wrong section

--- enable-discard-and-fstrim.sh
+++ enable-discard-and-fstrim.sh
@@ -1,5 +1,5 @@
 sudo awk -v OFS="\t" '{if (($3 ~ /^(swap|ext2|ext3|ext4|xfs|btrfs|f2fs|vfat|exfat|ntfs)$/) && !($4 ~ /discard/)) {$4 = ($4 ? $4 "," : "") "discard"} print}' /etc/fstab > /tmp/fstab.tmp && \
 sudo mv /tmp/fstab.tmp /etc/fstab && \
 echo -e "[Unit]\nDescription=Discard unused blocks once a week\n\n[Service]\nType=oneshot\nExecStart=/sbin/fstrim --all\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/fstrim.service && \
-echo -e "[Unit]\nDescription=Run fstrim weekly\n\n[Timer]\nOnCalendar=weekly\nPersistent=true\n\n[Install]\nWantedBy=timers.target\nRandomizedDelaySec=604800" | sudo tee /etc/systemd/system/fstrim.timer && \
+echo -e "[Unit]\nDescription=Run fstrim weekly\n\n[Timer]\nOnCalendar=weekly\nPersistent=true\nRandomizedDelaySec=1w\n\n[Install]\nWantedBy=timers.target" | sudo tee /etc/systemd/system/fstrim.timer && \
 sudo systemctl enable --now fstrim.timer
