0797b8dce6f3927a81afb006fae337a95a55b2d6
Mihkel Soomere <mihkel@soomere.ee>
2026-04-02 14:39:50 +0300

    expand oneliner to all supported filesystems

--- enable-discard-and-fstrim.sh
+++ enable-discard-and-fstrim.sh
@@ -1,6 +1,5 @@
-sudo awk -v OFS="\t" '{if (($3 == "swap" || $3 == "ext4" || $3 == "xfs" || $3 == "vfat") && !($4 ~ /discard/)) {$4 = ($4 ? $4 "," : "") "discard"} print}' /etc/fstab > /tmp/fstab.tmp && \
+sudo awk -v OFS="\t" '{if (($3 ~ /^(swap|ext2|ext3|ext4|xfs|btrfs|f2fs|vfat|exfat|ntfs)$/) && !($4 ~ /discard/)) {$4 = ($4 ? $4 "," : "") "discard"} print}' /etc/fstab > /tmp/fstab.tmp && \
 sudo mv /tmp/fstab.tmp /etc/fstab && \
 echo -e "[Unit]\nDescription=Discard unused blocks once a week\n\n[Service]\nType=oneshot\nExecStart=/sbin/fstrim --all\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/fstrim.service && \
 echo -e "[Unit]\nDescription=Run fstrim weekly\n\n[Timer]\nOnCalendar=weekly\nPersistent=true\n\n[Install]\nWantedBy=timers.target\nRandomizedDelaySec=604800" | sudo tee /etc/systemd/system/fstrim.timer && \
-sudo systemctl enable fstrim.timer && \
-sudo systemctl start fstrim.timer
+sudo systemctl enable --now fstrim.timer
